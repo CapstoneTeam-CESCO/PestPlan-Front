327b95ad5b8af87cfa57ef42c9c7f47c668865f1
baesh3744 <baesh3744@naver.com>
2021-01-10 17:56:14 +0900

    Add css for search input

--- src/components/molecules/BoardNav.jsx
+++ src/components/molecules/BoardNav.jsx
@@ -1,51 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { Chip, makeStyles } from '@material-ui/core';
-import { Search } from '@material-ui/icons';
+import { makeStyles, Chip, IconButton, InputAdornment, OutlinedInput, FormControl } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab';
 
 import './BoardNav.scss';
-import InputText from './../atoms/InputText';
 import PageBtn from './../atoms/PageBtn';
 
 const useStyles = makeStyles(() => ({
-    search: {
-        backgroundColor: '#333030',
-        borderRadius: '3px'
-    },
-    pageBtn: {
+    button_page: {
         backgroundColor: 'beige',
         minWidth: '10px',
         height: '35px',
         marginRight: '3px'
     },
-    curBtn: {
+    button_page_now: {
         backgroundColor: '#d9d4cf'
     },
-    toggle_buttons: {
+    buttonGroup_toggle: {
         display: 'flex',
         marginBottom: '4px',
         position: 'relative',
         right: '-20px'
     },
-    toggle_button: {
+    button_toggle: {
         height: '22px',
         border: 'none',
         padding: 0,
         margin: '1px 3px'
     },
-    toggle_chip: {
+    chip_toggle: {
         fontSize: '11px',
         height: '22px',
         cursor: 'pointer'
-    }
+    },
 }));
 
 function BoardNav(props) {
     const { className, page_total, page, setPage } = props;
     const [ pageBtns, setPageBtns ] = useState([]);
-    const [ values, setValues ] = useState([]);
+    const [ categories, setCategories ] = useState([]);
+    const [ query, setQuery ] = useState("");
     const classes = useStyles();
-    const categories = ['날짜', '지역', '설치 위치', '트랩 종류', '교체/에러'];
+    const searchCategories = ['날짜', '지역', '설치 위치', '트랩 종류', '교체/에러'];
 
     console.log("page in BoardNav");
 
@@ -53,8 +49,12 @@ function BoardNav(props) {
     const handleFirstClick = () => { setPage(1); }
     const handleLastClick = () => { setPage(page_total); }
 
-    const handleChange = (event, newValues) => {
-        setValues(newValues);
+    const handleChange = (event, newCategories) => {
+        setCategories(newCategories);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
     }
 
     useEffect(() => {
@@ -63,7 +63,7 @@ function BoardNav(props) {
         for(let cur = 1; cur <= page_total; cur++) {
             pageBtns.push(
                 <PageBtn
-                    className={`${classes.pageBtn} ${(cur === page) && classes.curBtn}`}
+                    className={`${classes.button_page} ${(cur === page) && classes.button_page_now}`}
                     key={cur}
                     contents={cur}
                     onClick={handleClick} />
@@ -75,50 +75,59 @@ function BoardNav(props) {
 
     return (
         <div className={className}>
-            <div className="pageBtns">
+            <div className="pagination">
                 <PageBtn
-                    className={classes.pageBtn}
+                    className={classes.button_page}
                     contents="<"
                     onClick={handleFirstClick} />
                 {pageBtns}
                 <PageBtn
-                    className={classes.pageBtn}
+                    className={classes.button_page}
                     contents=">"
                     onClick={handleLastClick} />
             </div>
-            <form className="search-form">
+
+            <form className="form-search" onSubmit={handleSubmit}>
                 {props.isNotice &&
                     <ToggleButtonGroup
-                        value={values}
+                        value={categories}
                         onChange={handleChange}
                         aria-label="search categories"
-                        className={classes.toggle_buttons}
+                        className={classes.buttonGroup_toggle}
                     >
-                        {categories.map(category => (
+                        {searchCategories.map(category => (
                             <ToggleButton
                                 key={category}
                                 value={category}
                                 aria-label={category}
-                                className={classes.toggle_button}
+                                className={classes.button_toggle}
                                 disableRipple={true}
                             >
-                                <Chip label={category} className={classes.toggle_chip} />
+                                <Chip label={category} className={classes.chip_toggle} />
                             </ToggleButton>
                         ))}
                     </ToggleButtonGroup>
                 }
-                <div>
-                    <InputText
-                        className="searchInput"
+
+                <FormControl variant="outlined">
+                    <OutlinedInput
+                        className="outlinedInput-search"
                         id="search"
-                        label="Search" />
-                    <Search
-                        className={classes.search}
-                        style={{ color: 'white', fontSize: 40 }} />
-                </div>
+                        value={query}
+                        onChange={ event => setQuery(event.target.value) }
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton type="submit" aria-label="search">
+                                    <SearchIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        } />
+                </FormControl>
             </form>
         </div>
     );
 }
 
 export default BoardNav;
+
+// variant="outlined"
